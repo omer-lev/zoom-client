9ea3fee65aa4dd2540af6110a86c4ea75a653bd0
omer lev <omerlev66@gmail.com>
2020-08-11 02:06:55 +0300

    final x6

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     secure: true,
     host: 'zoom-client-app.herokuapp.com',
-    port: '443'
+    port: '9000'
 })
 
 const myVideo = document.createElement('video')
