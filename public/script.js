8456c42b8bb4a4081526ba3507f1c6e35f006d32
omer lev <omerlev66@gmail.com>
2020-08-11 01:26:01 +0300

    zoom client

---
+++ public/script.js
@@ -0,0 +1,34 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
+})
+
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+    
+}
+
+socket.on('user-connected', userId => {
+    console.log("user connected: " + userId);
+})
+
+function addVideoStream(video, st
